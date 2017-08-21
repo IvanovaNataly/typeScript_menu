5740c95dc4232b3cbba44b1983a8ba71dbd8ea8d
IvanovaNataly <letter4natalia@gmail.com>
2017-08-21 22:41:30 +0300

    clear up

--- components/List.ts
+++ components/List.ts
@@ -9,14 +9,14 @@ export default class List implements userTestStatus{
     }
 
     render() {
-        let list = this._products.map(product: => {
-            `
-             <li class="collection-item">
-                 <span class="badge blue">${product: price}</span>
-                 <span class="new badge red"></span>Alan></li>
-           
-            
-            `
+        let list = this._products.map(product => {
+            //`
+            //  <li class="collection-item">
+            //      <span class="badge blue">${product: price}</span>
+            //      <span class="new badge red"></span>Alan></li>
+            //
+            //
+            // `
 
         })
     }
